88a134fb94085c28d60662a8571ae4ce3d149a7f
Abel Nascimento <suportabel.editing@gmail.com>
2026-01-06 21:31:15 -0300

    Primeiro passo da criação de interface gráfica: janela e botão.

--- src/itau_bank/system/ItauBank.java
+++ src/itau_bank/system/ItauBank.java
@@ -1,4 +1,5 @@
 package itau_bank.system;
+import itau_bank.system.security.login.interfaces.Window;
 import itau_bank.system.security.login.internalAcess.HTTPSection;
 import itau_bank.system.security.login.Login;
 import itau_bank.system.security.login.internalAcess.user_data.Database;
@@ -8,16 +9,18 @@ import itau_bank.system.security.register.Register;
 public class ItauBank {
     public static void main(String[] args) {
 
-        // attributes
-        Database db = new Database();
-        Register register = new Register(db);
-        Login login = new Login(db);
+//        // attributes
+//        Database db = new Database();
+//        Register register = new Register(db);
+//        Login login = new Login(db);
+//
+//        // Methods executions
+//        User user001 = register.register();
+//        User loggedUser = login.login();
+//        HTTPSection.acessAccount(loggedUser);
 
-        // Methods executions
-        User user001 = register.register();
-        User loggedUser = login.login();
-        HTTPSection.acessAccount(loggedUser);
-
+        Window window = new Window();
+        window.login();
 }
 }
 
--- src/itau_bank/system/security/Login/interfaces/Window.java
+++ src/itau_bank/system/security/Login/interfaces/Window.java
@@ -1,5 +1,19 @@
 package itau_bank.system.security.login.interfaces;
+import javax.swing.*;
 
 public class Window {
-    
+    public void login() {
+        // Window configurations
+        JFrame window = new JFrame();
+
+        final int WIDTH = 620;
+        final int HEIGHT = 480;
+
+        window.setSize(WIDTH, HEIGHT);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLayout(null);
+        window.add(Button.login());
+    }
 }
